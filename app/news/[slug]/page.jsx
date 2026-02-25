3f3763f3f3d5df975714f8e7cb99ce3ef705a408
omegastrike <115769802+omegastrike@users.noreply.github.com>
2026-02-26 00:24:25 +0530

    Comment out dynamic export in page.jsx
    Comment out the dynamic export in the news page.

--- app/news/[slug]/page.jsx
+++ app/news/[slug]/page.jsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 // import { notFound } from "next/navigation";
 // import { supabase } from "../../../lib/supabase";
