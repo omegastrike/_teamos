b2f3a87517636529632cdbe66235a7ea54baa258
Octane <115769802+omegastrike@users.noreply.github.com>
2026-03-03 23:43:23 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,65 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
-import BackgroundMusic from "@/components/BackgroundMusic";
-import CookieConsent from "../components/CookieConsent";
+import BackgroundMusic from "../components/BackgroundMusic";
 
 /* English main font */
 const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-inter",
+  variable: "font_telugu",
   display: "swap",
 });
 
 /* ✅ Local Telugu font */
 const teluguFont = localFont({
-  src: "./fonts/font_telugu.ttf", // MUST be inside /app/fonts
-  variable: "--font-telugu",
+  src: "../app/fonts/font_telugu.woff2", // MUST be inside /app/fonts
+  variable: "font_telugu",
   display: "swap",
 });
 
 export const metadata = {
-  title: "Omegastrike",
+  metadataBase: new URL("https://omegastrike.in"),
+  title: {
+    default: "Omegastrike Esports",
+    template: "%s | Omegastrike",
+  },
   description:
-    "Omegastrike is a professional BGMI esports organisation from India.",
+    "Omegastrike is a professional BGMI esports organization from India competing in national tournaments.",
+  keywords: [
+    "Omegastrike",
+    "BGMI team India",
+    "Esports India",
+    "BGMI roster",
+    "BGMI tournaments",
+    "Indian esports organization"
+  ],
+  openGraph: {
+    title: "Omegastrike Esports",
+    description:
+      "Professional BGMI esports organization from India.",
+    url: "https://omegastrike.in",
+    siteName: "Omegastrike",
+    images: [
+      {
+        url: "/og1.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_IN",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Omegastrike Esports",
+    description:
+      "Professional BGMI esports organization from India.",
+    images: ["/og1.jpg"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -29,12 +67,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.variable} ${teluguFont.variable}`}>
         <div
-    className="fixed inset-0 -z-10 bg-no-repeat bg-center bg-cover opacity-50"
+    className="fixed inset-0 -z-10 bg-no-repeat bg-center bg-cover opacity-10"
     style={{ backgroundImage: "url('/bg-texture.png')" }}
         />
         <BackgroundMusic />
         {children}
-        <CookieConsent />
       </body>
     </html>
   );
